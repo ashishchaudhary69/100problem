c44b5595333c6101e622e69551be74c08b8840e7
ashishchaudhary69 <ashish.chaudhary.mca.2025@miet.ac.in>
2026-02-24 18:20:24 +0530

    Add files via upload

---
+++ problem4.py
@@ -0,0 +1,11 @@
+# read inputs
+
+P = float(input())
+R = float(input())
+T = float(input())
+
+# calculate simple interest
+SI = (P *R * T) / 100
+
+# print result
+print("simple interest is:", SI)
