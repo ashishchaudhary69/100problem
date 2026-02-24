a23b651d77144eb10ea6624c564226510e0b251b
ashishchaudhary69 <ashish.chaudhary.mca.2025@miet.ac.in>
2026-02-25 00:31:42 +0530

    Add files via upload

---
+++ problem91.py
@@ -0,0 +1,16 @@
+s = input()
+result = ""
+new_word = True
+
+for ch in s:
+    if ch == " ":
+        result += ch
+        new_word = True
+    else:
+        if new_word:
+            result += ch.upper()
+            new_word = False
+        else:
+            result += ch.lower()
+
+print(result)
